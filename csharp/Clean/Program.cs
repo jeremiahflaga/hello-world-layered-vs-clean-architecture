30f717e41080486b27fb64e306c498cbc24304d4
Jboy Flaga <flaga.jeremiah@gmail.com>
2021-05-07 01:44:37 +0800

    "Hello World" Layers vs Clan Architecture in C#

---
+++ csharp/Clean/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Clean.Main
+{
+    using Clean.Data;
+    using Clean.Domain;
+    using Clean.Presentation;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greetingsRepository = new GreetingsRepository();
+            var greetingsService = new GreetingsService(greetingsRepository);
+            var greetingsController = new GreetingsController(greetingsService);
+
+            greetingsController.Execute();
+        }
+    }
+}
+
+namespace Clean.Presentation
+{
+    using Clean.Domain;
+
+    class GreetingsController
+    {
+        private readonly IGreetingsService greetingsService;
+
+        public GreetingsController(IGreetingsService greetingsService)
+        {
+            this.greetingsService = greetingsService;
+        }
+        public void Execute()
+        {
+            var greeting = greetingsService.Execute();
+            System.Console.WriteLine(greeting);
+        }
+    }
+}
+
+namespace Clean.Domain
+{
+    class GreetingsService : IGreetingsService
+    {
+        private readonly IGreetingsRepository greetingsRepository;
+
+        public GreetingsService(IGreetingsRepository greetingsRepository)
+        {
+            this.greetingsRepository = greetingsRepository;
+        }
+        public string Execute()
+        {
+            var greeting = greetingsRepository.CreateGreeting();
+            return greeting;
+        }
+    }
+
+    interface IGreetingsService
+    {
+        string Execute();
+    }
+
+    interface IGreetingsRepository
+    {
+        string CreateGreeting();
+    }
+}
+
+namespace Clean.Data
+{
+    using Clean.Domain;
+
+    class GreetingsRepository : IGreetingsRepository
+    {
+        public string CreateGreeting()
+        {
+            // Note: the greeting should read from a file or from a daabase, but for simplicity I will hard code it here
+            var greeting = "Hello World!";
+            return greeting;
+        }
+    }
+}
---
+++ csharp/Layered/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Layers.Main
+{
+    using Layers.Presentation;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greetingsController = new GreetingsController();
+            greetingsController.Execute();
+        }
+    }
+}
+
+namespace Layers.Presentation
+{
+    using Layers.Domain;
+
+    class GreetingsController
+    {
+        public void Execute()
+        {
+            var greetingsService = new GreetingsService();
+            var greeting = greetingsService.Execute();
+
+            System.Console.WriteLine(greeting);
+        }
+    }
+}
+
+namespace Layers.Domain
+{
+    using Layers.Data;
+
+    class GreetingsService
+    {
+        public string Execute()
+        {
+            var greetingsRepository = new GreetingsRepository();
+            var greeting = greetingsRepository.CreateGreeting();
+            return greeting;
+        }
+    }
+}
+
+namespace Layers.Data
+{
+    class GreetingsRepository
+    {
+        public string CreateGreeting()
+        {
+            // Note: the greeting should read from a file or from a daabase, but for simplicity I will hard code it here
+            var greeting = "Hello World!";
+            return greeting;
+        }
+    }
+}
